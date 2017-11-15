6c16bac40894152510e6c2555aa0ac1c31c12f7e
jimburk <Jim@JBurk.net>
2017-11-15 05:45:40 -0800

    Using buttons and an EditText. Logging button pushes.

--- app/src/main/java/edu/orangecoastcollege/pdavis11/cs273/capstone/winetastingrating/MainActivity.java
+++ app/src/main/java/edu/orangecoastcollege/pdavis11/cs273/capstone/winetastingrating/MainActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.graphics.Point;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -21,17 +23,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_scoring_6);
 
-        /*
-        Rating rate1 = new Rating(1, 1, 1, 1, 1, 1, 1, 1, 1, 1, "1");
-        Rating rate2 = new Rating(2, 1, 2, 2, 2, 2, 2, 2, 2, 2, "2");
-        Rating rate3 = new Rating(3, 2, 1, 3, 3, 3, 3, 3, 3, 3, "3");
-        Rating rate4 = new Rating(4, 2, 2, 3, 3, 3, 3, 3, 3, 4, "3");
-        ratingList.add(rate1);
-        ratingList.add(rate2);
-        ratingList.add(rate3);
-        ratingList.add(rate4);
-        */
-
         for (int i = 0; i < 5; i++) {
             for (int j = 0; j < 2; j++) {
                 Rating rating =
@@ -39,16 +30,22 @@ public class MainActivity extends AppCompatActivity {
                 ratingList.add(rating);
             }
         }
-
+/*
         Intent wineNotesIntent = getIntent();
         String varietalIntent = wineNotesIntent.getStringExtra("Varietal");
         String vintageIntent = wineNotesIntent.getStringExtra("Vintage");
         String wineryIntent = wineNotesIntent.getStringExtra("Winery");
         String vineyardIntent = wineNotesIntent.getStringExtra("Vineyard");
         String priceIntent = wineNotesIntent.getStringExtra("Price");
-
+*/
         ratingsListAdapter = new RatingListAdapter(this, R.layout.activity_wine_list, ratingList);
         ratingsListView = (ListView) findViewById(R.id.ratingListView);
         ratingsListView.setAdapter(ratingsListAdapter);
     }
+
+    public void colorFragment(View v) { Log.i("Main Activity", "colorFragment"); }
+    public void aromaFragment(View v) { Log.i("Main Activity", "aromaFragment"); }
+    public void bodyFragment(View v) { Log.i("Main Activity", "bodyFragment"); }
+    public void tasteFragment(View v) { Log.i("Main Activity", "tasteFragment"); }
+    public void finishFragment(View v) { Log.i("Main Activity", "finishFragment"); }
 }
